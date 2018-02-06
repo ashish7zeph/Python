664a9f0935604344fedccbecb4337dd23367f037
Ashish Pandey <31332047+ashish7zeph@users.noreply.github.com>
2018-02-06 18:47:40 +0000

    Delete global_statement.py

--- 3- functions/global_statement.py
+++
@@ -1,9 +0,0 @@
-def spam():
-    global eggs     # global eggs statement tells python, in this function, eggs refers to the global variable
-    eggs = 'spam'
-    
-eggs = 'global'
-spam()
-print(eggs)
-
-# output- spam
